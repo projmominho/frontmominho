d9ace7421647e82a54d89b7bc7fc11fa1d314d96
projmominho <projmominho@proton.me>
2025-05-15 06:01:40 -0300

    adicionado tailwind e daisyui

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="card w-96 bg-base-100 card-xl shadow-sm">
+      <div className="card-body">
+        <h2 className="card-title">Mominho a sua loja de bolinhos</h2>
+        <p>Em construção</p>
+        <div
+          className="justify-end card-actions"
+          onClick={() => window.open("https://github.com/projmominho/")}
+        >
+          <button className="btn btn-primary">Ver no github</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
